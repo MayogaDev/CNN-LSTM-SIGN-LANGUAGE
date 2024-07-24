7b7ec7707cc17111629239ff6cb8eb3a54d76687
andrealopezco20 <alopezco@unsa.edu.pe>
2024-07-24 09:47:58 -0500

    cambios

--- capture_samples.py
+++ capture_samples.py
@@ -58,6 +58,6 @@ def capture_samples(path, margin_frame=2, min_cant_frames=5):
         cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    word_name = "b"
+    word_name = "c"
     word_path = os.path.join(ROOT_PATH, FRAME_ACTIONS_PATH, word_name)
     capture_samples(word_path)
